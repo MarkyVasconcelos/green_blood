e62722f64c6beedd417b66024627b0c6e94fb1cf
= <=>
2012-11-13 23:20:30 -0200

    Display alert about no connectivity

--- GreenBlood/src/br/com/greenblood/RankingActivity.java
+++ GreenBlood/src/br/com/greenblood/RankingActivity.java
@@ -6,6 +6,8 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import br.com.digitalpages.commons.device.ConnectivityInfo;
+import br.com.digitalpages.commons.util.AlertUtil;
 
 public class RankingActivity extends Activity {
 	@Override
@@ -13,16 +15,21 @@ public class RankingActivity extends Activity {
 		super.onCreate(savedInstanceState);
 
 		setContentView(R.layout.ranking_activity);
-
+		
 		ListView rankingView = (ListView) findViewById(R.id.ranking_list_view);
 		ArrayAdapter<HighscoreDTO> arrayAdapter = new ArrayAdapter<HighscoreDTO>(
 				this, android.R.layout.simple_list_item_1);
-
+		
+		ConnectivityInfo.init(this);
+		if(ConnectivityInfo.isConnected()){
 		Random rdm = new Random();
 		for (int i = 0; i < 100; i++)
-			arrayAdapter.add(new HighscoreDTO(winners[rdm
-					.nextInt(winners.length)], 22400 + rdm.nextInt(20) * 100));
+			arrayAdapter.add(new HighscoreDTO(winners[rdm.nextInt(winners.length)], 22400 + rdm.nextInt(20) * 100));
+		
 		rankingView.setAdapter(arrayAdapter);
+		
+		} else
+			AlertUtil.showSimpleDialog(this, "Voce não está conectado na internet!");
 	}
 
 	private static final String[] winners = { 
