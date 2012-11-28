fcbe781bec81bf1ea2fc893920257892b9ab40c1
= <=>
2012-11-27 23:45:10 -0200

    Replace Enemy graphics

--- GreenBlood/src/br/com/greenblood/pieces/movable/Enemy.java
+++ GreenBlood/src/br/com/greenblood/pieces/movable/Enemy.java
@@ -4,70 +4,86 @@ import java.util.Random;
 
 import android.graphics.Bitmap;
 import android.graphics.Rect;
+import br.com.greenblood.core.GameCore;
 import br.com.greenblood.img.Sprite;
 import br.com.greenblood.util.ImageLoader;
 import br.com.greenblood.world.GameWorld;
 
 public class Enemy extends Character {
-    private static final Random rdm = new Random();
+	private static final Random rdm = new Random();
 
-    private final int maxHealth = 3;
-    private int currentHealth;
+	private final int maxHealth = 3;
+	private int currentHealth;
 
 	private final Sprite walkingSprite = new WalkingSprite();
 	private final Sprite punchingSprite = new PunchingSprite();
 
 	private boolean active = true;
-	
-    public Enemy(Rect bounds, Rect boundingBounds) {
-    	this(bounds, boundingBounds, 3, new WalkingSprite());
-    }
-    
-	protected Enemy(Rect bounds, Rect boundingBounds, int maxHealth, Sprite standingSprite) {
-        super(bounds, boundingBounds, 130 + rdm.nextInt(130), standingSprite);
-        setCollidable(true);
-        currentHealth = maxHealth;
-    }
 
-    @Override
-    public void processSelfLogics(long uptime) {
-    	if(!active)
-    		return;
-    	
-        Player player = GameWorld.player();
+	public Enemy(Rect bounds, Rect boundingBounds) {
+		this(bounds, boundingBounds, 2, new StandingSprite());
+	}
 
-        if(Math.abs(player.y() - y()) > 100)
-        	return;
-        
-        int dist = (int) (player.x() - x());
-        if (dist > 0 && dist < 100)
-            accelerate();
-        else if (dist < 0 && dist > -100)
-            desaccelerate();
-        
-        if(Math.abs(dist) < 40)
-        	punch();
-    }
+	protected Enemy(Rect bounds, Rect boundingBounds, int maxHealth,
+			Sprite standingSprite) {
+		super(bounds, boundingBounds, 130 + rdm.nextInt(130), standingSprite);
+		setCollidable(true);
+		currentHealth = maxHealth;
+	}
 
-    private static final class WalkingSprite extends Sprite {
-        public WalkingSprite(){
-            super(new Bitmap[] { ImageLoader.image("stick.png"), ImageLoader.image("stick_1.png") }, 200, true);
-        }
-    }
-    private final class PunchingSprite extends Sprite{
-        public PunchingSprite() {
-            super(new Bitmap[] { ImageLoader.image("stick_punch.png"), ImageLoader.image("stick_punch_1.png"), ImageLoader.image("stick_punch_2.png") },
-                    593, false);
-        }
-    }
-    
+	@Override
+	public void processSelfLogics(long uptime) {
+		if (!active)
+			return;
+
+		Player player = GameWorld.player();
+
+		int hundred = GameCore.pixels(150);
+		if (Math.abs(player.y() - y()) > hundred)
+			return;
+
+		int dist = (int) (player.x() - x());
+		if (dist > 0 && dist < hundred)
+			accelerate();
+		else if (dist < 0 && dist > -hundred)
+			desaccelerate();
+
+		if (Math.abs(dist) < GameCore.pixels(60))
+			punch();
+	}
+
+	private static final class StandingSprite extends Sprite {
+		public StandingSprite() {
+			super(new Bitmap[] { ImageLoader.image("enemy/walk_04.png") }, 200,
+					true);
+		}
+	}
+
+	private static final class WalkingSprite extends Sprite {
+		public WalkingSprite() {
+			super(new Bitmap[] { ImageLoader.image("enemy/walk_01.png"),
+					ImageLoader.image("enemy/walk_02.png"),
+					ImageLoader.image("enemy/walk_03.png"),
+					ImageLoader.image("enemy/walk_04.png") }, 200, true);
+		}
+	}
+
+	private final class PunchingSprite extends Sprite {
+		public PunchingSprite() {
+			super(new Bitmap[] { ImageLoader.image("enemy/punch_1.png"),
+					ImageLoader.image("enemy/punch_2.png"),
+					ImageLoader.image("enemy/punch_3.png"),
+					ImageLoader.image("enemy/punch_3.png") }, 593, false);
+		}
+	}
+
 	@Override
 	public void hit() {
 		currentHealth--;
-		
+
 		GameWorld.world().showEnemyStats(this);
-		
-		if(currentHealth == 0)
+
+		if (currentHealth == 0)
 			kill();
 	}
 
@@ -84,7 +100,7 @@ public class Enemy extends Character {
 	public int maxHealth() {
 		return maxHealth;
 	}
-	
+
 	public int currentHealth() {
 		return currentHealth;
 	}
